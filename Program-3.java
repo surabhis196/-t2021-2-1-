6c281a505b082d1634eec7ce59a7a2603a1aea84
surabhis196 <99898689+surabhis196@users.noreply.github.com>
2022-09-26 21:47:13 +0530

    Create Program-3.java
    Java program

---
+++ Program-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main	problem3
+{
+	public static void main(String[] args) {
+	    Scanner scan=new Scanner(System.in);
+		System.out.println("Enter a value");
+		int a=scan.nextInt();
+	
+		if(a%2==0){
+		    for(int i=1;i<=(a*2)-2;i++){
+		        if(i%2!=0){
+		    System.out.print(i);
+		}
+}
+	    }
+	    if(a%2!=0){
+		    for(int i=1;i<=a*2;i++){
+		        if(i%2!=0){
+		  System.out.print(i);
+		            }
+		       }  
+}
+	    }
+}
