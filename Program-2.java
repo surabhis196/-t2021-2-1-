2ecc8feb32a118de4fd473e85b63ca9982895cd6
surabhis196 <99898689+surabhis196@users.noreply.github.com>
2022-09-26 21:53:33 +0530

    Update Program-2.java
    Java  program

--- Program-2.java
+++ Program-2.java
@@ -5,14 +5,14 @@ public class Series
 	    Scanner scan=new Scanner(System.in);
 		System.out.println("Enter a value");
 		int a=scan.nextInt();
-		int c=1;
+		
 		
 		for(int i=1;i<=a*2;i++){
 		    if(i%2!=0){
 		    
 		    
 		        System.out.print(i);
-		        c++;
+		        
 		    }
 		        
 		}
